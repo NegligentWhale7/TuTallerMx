36d582f76d4344dc567541b42fc26ebe4ed29c70
Alam Armas <carlosalam33@hotmail.com>
2025-08-15 14:31:08 -0600

    Project creation

---
+++ tutallermxback/src/test/java/com/tutallermx/backend/tutallermxback/TutallermxbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutallermx.backend.tutallermxback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutallermxbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
