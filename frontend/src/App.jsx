79c0171c423ade00760968edf47762bacf36cf97
SilviaCanoS <silviacanosarabia@gmail.com>
2025-08-18 15:35:39 -0600

    Display all vehicles in home page app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
+import Article from "./components/article";
+import Vehicle from "./components/vehicle";
+import {mockVehicles} from "./data/data.js";
+const vehicles = mockVehicles;
+
 function App() {
 
   return (
-    <>
-    </>
+    <div id="principal">
+      <Article /> 
+      <main>
+        {vehicles.map(vehicle => <Vehicle onAddVehicle={() => {
+          setShowForm(true);
+          setSelectedVehicle(vehicle);
+        }} key={vehicle.id} vehicleInfo={vehicle}/>)}
+      </main>
+    </div>
   )
 }
 
