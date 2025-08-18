fa9b7e7793395d0c6e865b8f24bbccf1359a223c
alberto.perez <alberto.perez@solera.com>
2025-08-18 15:54:09 -0600

    Filter of Vehicle by Make, Service and Status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import Article from "./components/article";
 import Vehicle from "./components/vehicle";
+import Navbar from "./components/navbar";
 import {mockVehicles} from "./data/data.js";
 const vehicles = mockVehicles;
 
 function App() {
 
+  const [makeFilter, setMakeFilter] = useState("");
+  const [serviceFilter, setServiceFilter] = useState("");
+  const [statusFilter, setStatusFilter] = useState("");
+
   return (
     <div id="principal">
       <Article /> 
+      <Navbar makeFilter={makeFilter} setMakeFilter={setMakeFilter} serviceFilter={serviceFilter} 
+
+        setServiceFilter={setServiceFilter} statusFilter={statusFilter} setStatusFilter={setStatusFilter} />
       <main>
-        {vehicles.map(vehicle => <Vehicle onAddVehicle={() => {
+        {vehicles.filter(vehicle => Vehicle.status !== "Done" && (makeFilter === "" || vehicle.make === makeFilter) 
+          && (serviceFilter === "" || vehicle.service === serviceFilter) 
+          && (statusFilter === "" || vehicle.status === statusFilter)).map(vehicle => <Vehicle onAddVehicle={() => {
           setShowForm(true);
           setSelectedVehicle(vehicle);
         }} key={vehicle.id} vehicleInfo={vehicle}/>)}
