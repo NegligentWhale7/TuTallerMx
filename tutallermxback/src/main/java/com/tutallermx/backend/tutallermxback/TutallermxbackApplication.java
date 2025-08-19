523cca8824f7be49353a022b1983c1e29346d383
Alam Armas <carlosalam33@hotmail.com>
2025-08-19 08:53:14 -0600

    Stand up for a new spring boot project

---
+++ tutallermxback/src/main/java/com/tutallermx/backend/tutallermxback/TutallermxbackApplication.java
@@ -0,0 +1,13 @@
+package com.tutallermx.backend.tutallermxback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutallermxbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutallermxbackApplication.class, args);
+	}
+
+}
